d01e81e86d40123ad7af06f422157c44da9a95cb
Thijs Borst <thijs.borst@incentro.com>
2015-08-18 19:58:00 +0200

    Updated AssemblyInfo.

--- source/DD4T.DI.Unity/Properties/AssemblyInfo.cs
+++ source/DD4T.DI.Unity/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("DD4T.Bootstrap.Unity")]
+[assembly: AssemblyTitle("DD4T.DI.Unity")]
 [assembly: AssemblyProduct("DD4T")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Open Source")]
