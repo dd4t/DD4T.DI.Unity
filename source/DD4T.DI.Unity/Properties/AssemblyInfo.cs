67fd0d9c22e0679e5d297ea2e6b763e05283ef20
Thijs Borst <thijs.borst@incentro.com>
2015-08-18 19:52:31 +0200

    Implemented CI and updated configuration.

--- source/DD4T.DI.Unity/Properties/AssemblyInfo.cs
+++ source/DD4T.DI.Unity/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DD4T.DI.Unity")]
-[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyTitle("DD4T.Bootstrap.Unity")]
+[assembly: AssemblyProduct("DD4T")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DD4T.DI.Unity")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Open Source")]
+[assembly: AssemblyCopyright("Open Source Apache 2.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("872f961b-88e7-4a64-ba3c-9d809e236ba8")]
+[assembly: Guid("2c95e09e-03b5-4a6d-8b19-6a46e38840ff")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha1")]
